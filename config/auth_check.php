2fbd328937b6416f0d93db5df21d3a52b91e7062
Rafiatou Malam Ali <rafiatoumalamali@gmail.com>
2025-11-20 17:44:52 +0000

    updates

--- config/auth_check.php
+++ config/auth_check.php
@@ -3,13 +3,13 @@ session_start();
 
 // Check if user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header('Location: /Attendance_manager/UpdatedFiles/index.php');
+    header('Location: /~rafiatou.ali/index.php');
     exit;
 }
 
 // Check if user is approved
 if (!isset($_SESSION['status']) || $_SESSION['status'] !== 'approved') {
-    header('Location: /Attendance_manager/UpdatedFiles/pending_approval.php');
+    header('Location: /~rafiatou.ali/pending_approval.php');
     exit;
 }
 
@@ -19,16 +19,16 @@ function checkRole($allowed_roles) {
         // Redirect to their actual dashboard based on their real role
         switch ($_SESSION['role']) {
             case 'student':
-                header('Location: /Attendance_manager/UpdatedFiles/Student_Dashboard.php');
+                header('Location: /~rafiatou.ali/Student_Dashboard.php');
                 break;
             case 'faculty':
-                header('Location: /Attendance_manager/UpdatedFiles/Faculty_Dashboard.php');
+                header('Location: /~rafiatou.ali/Faculty_Dashboard.php');
                 break;
             case 'faculty_intern':
-                header('Location: /Attendance_manager/UpdatedFiles/FI_Dashboard.php');
+                header('Location: /~rafiatou.ali/FI_Dashboard.php');
                 break;
             default:
-                header('Location: /Attendance_manager/UpdatedFiles/index.php');
+                header('Location: /~rafiatou.ali/index.php');
         }
         exit;
     }
